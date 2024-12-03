31f5d11dc284263e161d40971aa4201dab440b23
Ali Bennouna Louridi <96294983+Bouchta003@users.noreply.github.com>
2024-12-04 00:35:17 +0100

    feat(indicator):visible E_Key indicator on top of storables

--- Escape Game/Assets/Scripts/Player/Interaction/E_Key_Indicator.cs
+++ Escape Game/Assets/Scripts/Player/Interaction/E_Key_Indicator.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class E_Key_Indicator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 50f; // Speed of rotation in degrees per second
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotate the cube around its Y-axis
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
--- Escape Game/Assets/Scripts/Player/Interaction/StorableDetector.cs
+++ Escape Game/Assets/Scripts/Player/Interaction/StorableDetector.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class StorableDetector : MonoBehaviour
 {
+    public GameObject prefabToInstantiate; // Assign your prefab in the Inspector
+    private GameObject currentPrefabInstance; // Tracks the currently instantiated prefab
+    [SerializeField] float prefabHeight = 2f;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Storable"))
         {
-            Debug.Log(other.gameObject.name+ " is storable");
+            if (currentPrefabInstance != null)
+            {
+                Destroy(currentPrefabInstance);
+            }
+
+            // Instantiate a new prefab above the collided object
+            Vector3 spawnPosition = other.transform.position + Vector3.up * prefabHeight;
+            currentPrefabInstance = Instantiate(prefabToInstantiate, spawnPosition, Quaternion.identity);
         }
     }
 }
-
