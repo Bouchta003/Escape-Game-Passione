8ff46140045b694b5b35463621dad7bb0079e620
Joyce <lianajoycemarie@gmail.com>
2025-01-07 13:31:18 +0100

    fix: create variables inside if statement

--- Escape Game/Assets/Scripts/Puzzles/AnodeCathode/AnodeCathodePuzzle.cs
+++ Escape Game/Assets/Scripts/Puzzles/AnodeCathode/AnodeCathodePuzzle.cs
@@ -56,12 +56,11 @@ public class AnodeCathodePuzzle : MonoBehaviour, IInteractable
         MaterialData anode = anodeSlot.AssignedMaterial;
         MaterialData cathode = cathodeSlot.AssignedMaterial;
 
-
-        string anodeName = anode.materialName;
-        string cathodeName = cathode.materialName;
-
         if (anode != null && cathode != null)
         {
+            string anodeName = anode.materialName;
+            string cathodeName = cathode.materialName;
+
             if (anodeName == "Graphite" && cathodeName == "Cobalt")
             {
                 Debug.Log("Correct Combination! Door Unlocks.");
