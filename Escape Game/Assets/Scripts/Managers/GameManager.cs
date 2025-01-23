853efe339d2185be6874bd38145719514288813e
Joyce <lianajoycemarie@gmail.com>
2025-01-23 11:24:58 +0100

    fix(end): enable cursor use

--- Escape Game/Assets/Scripts/Managers/GameManager.cs
+++ Escape Game/Assets/Scripts/Managers/GameManager.cs
@@ -6,6 +6,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Player_Inventory.inventory.Contains("USBKey")) SceneManager.LoadScene("Outro");
+        if (Player_Inventory.inventory.Contains("USBKey"))
+        {
+            EnableCursor();
+            SceneManager.LoadScene("Outro");
+        }
     }
+
+    /// <summary>
+    /// Enables the cursor for user interaction.
+    /// </summary>
+    void EnableCursor()
+    {
+        Cursor.lockState = CursorLockMode.None; // Unlock cursor
+        Cursor.visible = true; // Make cursor visible
+    }
+
 }
